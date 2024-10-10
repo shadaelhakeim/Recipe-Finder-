cef1260af3a372222a513662298b880ca34a4809
shada elhakeim <shadaelhakeim@gmail.com>
2024-10-10 19:13:32 +0300

    Fix GitHub Pages routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 // import ThemeCustomize from "./components/themeCustomize"
 import HomePage from "./components/HomePage"
 import FavoritesPage from "./components/FavoritesPage"
-import { Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import RecipeDetails from "./components/RecipeDetails";
 import RecipePage from "./components/RecipePage";
 import AboutUs from "./components/AboutUS";
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
       <div >
-      
+       <Router  basename="/Recipe-Finder-App">
       <Routes>
         <Route path="/Home" element={<HomePage />} />
         <Route path="/Recipes" element={<RecipePage />} />
@@ -19,7 +19,8 @@ function App() {
         <Route path="/recipe/:id" element={<RecipeDetails />} /> 
         <Route path="/About" element={<AboutUs />} /> 
         
-      </Routes>
+          </Routes>
+          </Router>
       </div>
       
       </>
